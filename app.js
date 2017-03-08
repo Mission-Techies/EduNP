9b478abb984de416df80728c0ede56596f74816e
zurigo <gonzalezsadai.z@gmail.com>
2017-03-07 23:10:36 -0800

    css

---
+++ app.js
@@ -0,0 +1,20 @@
+var edunp_data_url = 'https://api.airtable.com/v0/appQ8einDCzZyfJYO/Agency?api_key=keyFQuPxdLW9FdIUb';
+var edunpHTML = '';
+var edunpDiv = $('.Agency');
+var renderedunpData = function(data) {
+  data.records.forEach(function(program) {
+    edunpHTML += '<h2>' + program.fields['Name'] + '</h2>';
+    edunpHTML += '<a href="' + program.fields['Extension'] + '">' + program.fields['Extension'] + '</a>';
+    edunpHTML += '<p>' + program.fields['Description'] + '</p>';
+    if (program.fields['Email']) {
+      edunpHTML += '<p>' + program.fields['Email'] + '</p>';
+      edunpHTML += '<p>' + program.fields['Volunteer'] + '</p>';
+    }
+
+    edunpHTML += '<hr />';
+  });
+  edunpDiv.html(edunpHTML);
+};
+
+//
+$.getJSON(edunp_data_url, renderedunpData);
